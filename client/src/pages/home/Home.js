fd5a82f5fc8ee13ba38e0d6de54c0ad8ef2823bf
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-24 03:37:28 +0300

    Displayed Featured component on the home page section

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 
@@ -7,10 +8,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Home;
