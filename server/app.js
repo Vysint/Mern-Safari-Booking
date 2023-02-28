9be356ea11259f5a301dbfe88321fc4c2b200faf
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-28 03:03:08 +0300

    Connected the app to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
+// mongoose.set("strictQuery", false);
 
-app.listen(8800, () => {
-  console.log("Connected to backend")
-})
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log("Connected to be Backend");
+});
