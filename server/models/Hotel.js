cdd991ce37ec0a4dde46de23957a029f9d1a0f3f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-28 14:40:26 +0300

    Modified th Hotel model schema

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -28,6 +28,10 @@ const HotelSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  title: {
+    type: String,
+    required: true,
+  },
   rating: {
     type: Number,
     min: 0,
