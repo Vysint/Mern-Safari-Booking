73e5d15b388faebe1ef72fa8aed835f06fe88b6f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-28 15:38:36 +0300

    added CRUD hotel methods

--- server/controllers/HotelController.js
+++ server/controllers/HotelController.js
@@ -1,5 +1,3 @@
-const express = require("express");
-
 const Hotel = require("../models/Hotel");
 
 //CREATE
@@ -10,6 +8,49 @@ exports.createHotel = async (req, res) => {
   try {
     const savedHotel = await newHotel.save();
     res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.updateHotel = async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.deleteHotel = async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getHotel = async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getAllHotels = async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json({ hotels: hotels });
   } catch (err) {
     res.status(500).json(err);
   }
