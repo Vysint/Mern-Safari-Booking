b333a221a060c6fae664b4757c696f0c96bd2413
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-28 17:04:08 +0300

    Customized error middleware

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const usersRoute = require("./routes/usersRoute");
 const hotelsRoute = require("./routes/hotelsRoute");
 const roomsRoute = require("./routes/roomsRoute");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 dotenv.config();
 
@@ -16,6 +18,24 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+//Missing route
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find the route", 404);
+  throw error;
+});
+
+// Error middleware
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errormessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errormessage,
+    stack: err.stack,
+  });
+});
 
 const connect = async () => {
   try {
