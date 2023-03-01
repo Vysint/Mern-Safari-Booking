01620ab1c00631406d60914011e6025d488b0d54
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-01 11:01:54 +0300

    Updated error handling in the hotelcontroller to use the error middleware

--- server/controllers/HotelController.js
+++ server/controllers/HotelController.js
@@ -1,19 +1,19 @@
 const Hotel = require("../models/Hotel");
+const HttpError = require("../models/error");
 
 //CREATE
-
-exports.createHotel = async (req, res) => {
+exports.createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
 
   try {
     const savedHotel = await newHotel.save();
     res.status(200).json(savedHotel);
   } catch (err) {
-    res.status(500).json(err);
+    return next(new HttpError("Something went wrong, try again later", 500));
   }
 };
 
-exports.updateHotel = async (req, res) => {
+exports.updateHotel = async (req, res, next) => {
   try {
     const updatedHotel = await Hotel.findByIdAndUpdate(
       req.params.id,
@@ -25,33 +25,37 @@ exports.updateHotel = async (req, res) => {
 
     res.status(200).json(updatedHotel);
   } catch (err) {
-    res.status(500).json(err);
+    return next(new HttpError("Updatedn failed, try again later", 500));
   }
 };
 
-exports.deleteHotel = async (req, res) => {
+exports.deleteHotel = async (req, res, next) => {
   try {
     await Hotel.findByIdAndDelete(req.params.id);
     res.status(200).json("Hotel deleted!");
   } catch (err) {
-    res.status(500).json(err);
+    return next(new HttpError("Delete failed, try again later", 500));
   }
 };
 
-exports.getHotel = async (req, res) => {
+exports.getHotel = async (req, res, next) => {
   try {
     const hotel = await Hotel.findById(req.params.id);
     res.status(200).json(hotel);
   } catch (err) {
-    res.status(500).json(err);
+    return next(
+      new HttpError("Could not find a hotel for the provided id", 500)
+    );
   }
 };
 
-exports.getAllHotels = async (req, res) => {
+exports.getAllHotels = async (req, res, next) => {
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({});
     res.status(200).json({ hotels: hotels });
   } catch (err) {
-    res.status(500).json(err);
+    return next(
+      new HttpError("Something went wrong, could not retrieve the data", 500)
+    );
   }
 };
