e4cce9676bb4942c0dc48f92ccdc8b4081898f45
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-03 11:33:29 +0300

    Added CRUD operartions for users - in users controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,44 +1,51 @@
-const bcrypt = require("bcryptjs");
-
 const User = require("../models/User");
 const HttpError = require("../models/error");
 
-exports.register = async (req, res, next) => {
+exports.createUser = async (req, res, next) => {
+  const newUser = new User(req.body);
+
   try {
-    const salt = await bcrypt.genSalt(12);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.status(200).json({ "Registered User": newUser });
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
   } catch (err) {
-    return next(new HttpError("Failed to register user, try again later", 500));
+    return next(new HttpError("Signing up user failed!", 404));
+  }
+};
+exports.updateUser = async (req, res, next) => {
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (err) {
+    return next(new HttpError("Failed to update user!", 500));
   }
 };
 
-exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
-  let existingUser;
+exports.deleteUser = async (req, res, next) => {
   try {
-    existingUser = await User.findOne({ email: email });
-    if (!existingUser) {
-      return next(new HttpError("User not found!", 404));
-    }
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-    if (!isPasswordValid) {
-      return next(new HttpError("Wrong Password!", 400));
-    }
-    // const {password, isAdmin, ...otherDetails} = existingUser._doc
-    res
-      .status(200)
-      .json({ userId: existingUser.id, email: existingUser.email });
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
   } catch (err) {
-    return next(new HttpError("Login in failed, try again later.", 500));
+    return next(new HttpError("Failed to delete user!", 500));
+  }
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    return next(new HttpError("Failed to fetch user!", 500));
+  }
+};
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    return next(new HttpError("Fetching users failed!", 500));
   }
 };
