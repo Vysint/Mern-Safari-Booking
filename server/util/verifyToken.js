a399b1709bfbe0920fca957efa56a6eaedbbcbc6
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-05 14:30:01 +0300

    Added token signing

---
+++ server/util/verifyToken.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/error");
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return next(new HttpError("You are not authenticated!", 401));
+  }
+
+  jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+    if (err) return next(new HttpError("Token is not valid", 403));
+    req.user = user;
+    next();
+  });
+};
+
+exports.verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(new HttpError("You are not authorized!", 403));
+    }
+  });
+};
+
+exports.verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      new HttpError("You are not authorized!", 403);
+    }
+  });
+};
