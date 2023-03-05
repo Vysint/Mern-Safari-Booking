03fc41b26b400bb1dee241fddf3a36659a2d012d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-05 14:30:35 +0300

    Added token verification for some routes

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,10 +1,29 @@
 const express = require("express");
 
 const userControllers = require("../controllers/UserController");
+const tokenControllers = require("../util/verifyToken");
 
 const router = express.Router();
 
-// router.post("/", userControllers.createUser);
+router.get(
+  "/checkauthentication",
+  tokenControllers.verifyToken,
+  (req, res, next) => {
+    res.send("Hello user, you are logged in");
+  }
+);
+
+router.get("/checkuser/:id", tokenControllers.verifyUser, (req, res, next) => {
+  res.send("Hello user, you are logged in and you can delete your account");
+});
+
+router.get(
+  "/checkadmin/:id",
+  tokenControllers.verifyAdmin,
+  (req, res, next) => {
+    res.send("Hello admin, you are logged in and you can delete all accounts.");
+  }
+);
 
 // UPDATE
 router.put("/:id", userControllers.updateUser);
