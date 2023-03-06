d5c027305117aa2053af6583493a10b777b0f80e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-06 14:20:30 +0300

    Added CRUD operartions for rooms - in rooms controller

--- server/routes/roomsRoute.js
+++ server/routes/roomsRoute.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { verifyAdmin } = require("../util/verifyToken");
+const roomControllers = require("../controllers/roomController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Rooms endpoint!");
-});
+router.post("/", verifyAdmin, roomControllers.createRoom);
+
+router.put("/:id", verifyAdmin, roomControllers.updateRoom);
+
+router.delete("/:id", verifyAdmin, roomControllers.deleteRoom);
+
+router.get("/:id", roomControllers.getRoom);
+
+router.get("/", roomControllers.getRooms);
 
 module.exports = router;
