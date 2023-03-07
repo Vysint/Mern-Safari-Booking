4a2b242ac919b2b2b8e0e98d4d53b698b43da9ff
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-07 13:40:48 +0300

    Fetched hotels with query conditions

--- server/controllers/HotelController.js
+++ server/controllers/HotelController.js
@@ -50,8 +50,12 @@ exports.getHotel = async (req, res, next) => {
 };
 
 exports.getAllHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find({});
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json({ hotels: hotels });
   } catch (err) {
     return next(
