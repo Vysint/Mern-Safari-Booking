164c6a38d22961586d2ba1ea5cb3362f450f07a1
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-07 13:26:22 +0300

    Bugs Fixed

--- client/src/components/propertyList/PropertyList.js
+++ client/src/components/propertyList/PropertyList.js
@@ -1,12 +1,8 @@
-import React from "react";
 import useFetch from "../../hooks/useFetch";
-
 import "./PropertyList.css";
 
 const PropertyList = () => {
-  const { data, loading, error } = useFetch(
-    "http://localhost:8800/api/hotels/countByType"
-  );
+  const { data, loading, error } = useFetch("http://localhost:8800/api/hotels/countByType");
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -18,20 +14,23 @@ const PropertyList = () => {
   return (
     <div className="pList">
       {loading ? (
-        "Loading..."
+        "loading"
       ) : (
         <>
-          {data && images.map((img, i) => (
-            <div className="pListItem" key = {i}>
-              <img src={img} alt="" className="pListImg" />
-              <div className="pListTitles">
-                <h1>{data[i].type}</h1>
-                <h2>
-                  {data[i].count} {data[i].type}
-                </h2>
+          {data &&
+            images.map((img,i) => (
+              <div className="pListItem" key={i}>
+                <img
+                  src={img}
+                  alt=""
+                  className="pListImg"
+                />
+                <div className="pListTitles">
+                  <h1>{data[i]?.type}</h1>
+                  <h2>{data[i]?.count} {data[i]?.type}</h2>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </>
       )}
     </div>
