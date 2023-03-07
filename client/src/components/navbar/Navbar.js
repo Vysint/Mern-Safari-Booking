f318fa6fecd1b43a4a8efdc3a26f72d9d7cc4f5b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-07 13:41:31 +0300

    Linked the logo to the home page

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 
@@ -6,7 +7,9 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">safaribooking</span>
+        <Link to="/" style={{color: "inherit", textDecoration: "none"}}>
+          <span className="logo">safaribooking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
