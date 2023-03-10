ec78ffe756da6674f18e943390c8bce21ca0a82c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-11 01:33:38 +0300

    Set up localstorage to store user after successful signing in

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
 };
@@ -41,9 +41,9 @@ const AuthReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-  // useEffect(() => {
-  //   localStorage.setItem("user", JSON.stringify(state.user));
-  // }, [state.user]);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
 
   return (
     <AuthContext.Provider
