16a05e35530c776792fc8765e89b40348d4d3139
Vincent Kipngetich <vinniebluz@icloud.com>
2023-03-15 00:53:20 +0300

    Corrected props naming

--- client/src/pages/hotel/Hotel.js
+++ client/src/pages/hotel/Hotel.js
@@ -150,7 +150,7 @@ const Hotel = () => {
           <Footer />
         </div>
       )}
-      {openModal && <Reserve setOpen={setOpenModal} hotelid={id} />}
+      {openModal && <Reserve setOpen={setOpenModal} hotelId={id} />}
     </div>
   );
 };
